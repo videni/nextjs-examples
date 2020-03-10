2584c79bdb13310eabcc1cf4d019d01d96afffcf
Vidy Videni <vidy.videni@gmail.com>
2020-03-10 10:26:58 +0800

    The server/index.ts is not packed by webpack, that is why @ alias doesn't work

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,6 @@
 import { createServer } from 'http';
 import next from 'next';
-import routes from '@/routes';
+import routes from '../routes';
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
